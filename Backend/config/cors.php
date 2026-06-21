bd8cd1ec7a4b1ee219ec2a1de47c52fd5dcf3d0c
Anirban Das <anirban140963@gmail.com>
2026-06-21 12:32:07 +0530

    fix on cors to support vercel

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -19,7 +19,12 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'https://community-sharing-platform.vercel.app',
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'http://127.0.0.1:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +34,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
